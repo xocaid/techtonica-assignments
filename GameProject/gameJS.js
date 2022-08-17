8ed2dca851c830897881885948d19a75117a9b03
xocaid <xocaid@gmail.com>
2022-08-16 20:22:52 -0700

    adding Game Project

---
+++ GameProject/gameJS.js
@@ -0,0 +1,13 @@
+//GAME: Guess my Number
+//INPUt: guess number between 1-100
+//create a button to submit guess
+//print/display a RANDOM number between 1-100
+//IF guessed correctly : print onto page congratulations, you guessed numX
+//IF guessed incorrectly: print onto page better luck next time
+//create a button to refresh everytime game concludes
+
+
+//will need to get a random number use Math.floor & math.Random
+//will need if/else statement 
+//print to console/page
+//eventListener
