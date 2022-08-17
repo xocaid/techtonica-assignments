450ae5fe52829c6306ae75ae692218bae7848183
xocaid <xocaid@gmail.com>
2022-08-17 09:47:20 -0700

    add gamefolder

--- GameProject/gameJS.js
+++ GameProject/gameJS.js
@@ -11,3 +11,33 @@
 //will need if/else statement 
 //print to console/page
 //eventListener
+
+function guessANumber(){
+  //generating the program's random number
+  let randomNum = Math.floor(Math.random() * 100)
+  //user's input, changed to a Number
+  let inputNum = Number(userGuess);
+
+//IF Statement if guessed correctly/incorrectly
+  if(inputNum === randomNum){
+    return "Congratulations you correctly guessed" + randomNum; 
+  }else{
+    return "Better luck next time!"
+  }
+}
+guessANumber()
+
+//create BREAK 
+let br = document.createElement("br");
+
+//Add input text box
+let userInputBox = document.createElement("INPUT");
+userInputBox.setAttribute('type', 'text');
+userInputBox.setAttribute('placeholder', 'Enter your guess');
+document.body.appendChild(userInputBox);
+
+//Add Submit Button
+let submitBtn = document.createElement('INPUT');
+submitBtn.setAttribute('type', 'submit');
+submitBtn.setAttribute('value', 'Submit Guess');
+document.body.appendChild(submitBtn);
