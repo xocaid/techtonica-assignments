24e59a3f04503693c5c9c84da1e81fed38773d10
xocaid <xocaid@gmail.com>
2022-08-17 19:21:46 -0700

    Built JS DOM Elements

--- GameProject/gameJS.js
+++ GameProject/gameJS.js
@@ -1,49 +1,81 @@
 //GAME: Guess my Number
 //INPUt: guess number between 1-100
-//create a button to submit guess
+//number cannot be lower than 0 or greater than 100
 //print/display a RANDOM number between 1-100
 //IF guessed correctly : print onto page congratulations, you guessed numX
 //IF guessed incorrectly: print onto page better luck next time
-//create a button to refresh everytime game concludes
 
 
-//will need to get a random number use Math.floor & math.Random
-//will need if/else statement 
-//print to console/page
-//eventListener
+//console.log to see what part of the code its reaching & for HTML
+//can create a submit button w. form or create a button and add event listener to it
 
-function guessANumber(){
-  //generating the program's random number
-  let randomNum = Math.floor(Math.random() * 100)
-  //user's input, changed to a Number
-  let inputNum = Number(userGuess);
+// function guessANumber() {
+//   //generating the program's random number
+//   let randomNum = Math.floor(Math.random() * 100)
+//   console.log(randomNum)
+//   //user's input, changed to a Number
+//   let inputNum = Number(userGuess);
 
-//IF Statement if guessed correctly/incorrectly
-  if(inputNum === randomNum){
-    return "Congratulations you correctly guessed" + randomNum; 
-  }else{
-    return "Better luck next time!"
-  }
-}
-guessANumber()
+//   //IF Statement if guessed correctly/incorrectly
+//   if (inputNum === randomNum) {
+//     return "Congratulations you correctly guessed" + randomNum;
+//   } else {
+//     return "Better luck next time!"
+//   }
+// }
+// console.log(guessANumber())
 
 //create BREAK 
 let br = document.createElement("br");
 
-//Add input text box
+//*********DIV**************8
+let gameDiv = document.createElement('div');
+gameDiv.setAttribute('id', 'myGameDiv');
+gameDiv.setAttribute('class', 'gameDivContent');
+document.body.appendChild(gameDiv);
+
+//*******FORM*********
+let gameForm = document.createElement("FORM");
+gameForm.setAttribute('id', 'myGameForm');
+myGameDiv.appendChild(gameForm);
+
+//Add User Guessinput text box
 let userInputBox = document.createElement("INPUT");
 userInputBox.setAttribute('type', 'text');
+userInputBox.setAttribute('id', 'userGuess');
 userInputBox.setAttribute('placeholder', 'Enter your guess');
-document.body.appendChild(userInputBox);
+userInputBox.setAttribute('required', '');
+document.getElementById('myGameForm').appendChild(userInputBox);
 
-//Add Submit Button
+//Create Submit Button
 let submitBtn = document.createElement('INPUT');
 submitBtn.setAttribute('type', 'submit');
-submitBtn.setAttribute('value', 'Submit Guess');
-document.body.appendChild(submitBtn);
+submitBtn.setAttribute('value', 'Submit Your Guess');
+gameForm.appendChild(br.cloneNode());
+gameForm.appendChild(submitBtn);
 
-//Add Submit Button
-let submitBtn1 = document.createElement('INPUT');
-submitBtn1.setAttribute('type', 'submit1');
-submitBtn1.setAttribute('value', 'Submit Guess1');
-document.body.appendChild(submitBtn1);
+//Create Reset Button
+let resetBtn = document.createElement('INPUT');
+resetBtn.setAttribute('type', 'reset');
+resetBtn.setAttribute('id', 'resetGuess');
+resetBtn.setAttribute('value', 'Reset');
+gameForm.appendChild(br.cloneNode());
+gameForm.appendChild(resetBtn);
+
+//******PARAGRAPH******
+let gameP = document.createElement('p');
+gameP.setAttribute('id', 'myGameP');
+gameDiv.appendChild(gameP);
+
+//Add eventListener to Submit Button
+gameForm.addEventListener('submit', (e)=>{
+e.preventDefault();
+  let userGuessPrint = document.getElementById('userGuess').value;
+  let printTextPg = document.querySelector('p');
+  printTextPg.innerHTML= `${userGuessPrint}`;//Prints user's Guess to page
+});
+
+//Reset Method for Reset Button
+document.getElementById('resetGuess').reset();
+
+
