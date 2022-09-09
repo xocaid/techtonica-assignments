859a383aa4e2fa76959bccfd03f4e6c1e4a9818f
xocaid <xocaid@gmail.com>
2022-09-09 12:24:38 -0700

    Changes to API from Kelvin to F

--- weatherAPIApp/client/src/components/weather.js
+++ weatherAPIApp/client/src/components/weather.js
@@ -1,15 +1,13 @@
 //THIS IS FRONT END
-//students.js is where you are creating the information that will be displayed in the app.js page
-//in order to see the server & client on localhost, it has to be called npm start in the folder
+//IMPORT useEffect to use, can be placed with useState
 import { useState, useEffect } from "react";
 
 const Weather = () => {
   const [weather, setWeather] = useState(null);
 
   //anonymous function to FETCH data
-  //URL comes from the API you are trying to access information from
-  //response is what you are trying to get from API
-  //data is the information you received from API
+  //ROUTE is where the API data will diplayed
+  //DATA will be loaded/stored in /api/weather
   const loadData = () => {
     fetch('http://localhost:8001/api/weather')
       .then((response) => response.json())
@@ -29,7 +27,6 @@ const Weather = () => {
     return <div>loading...</div>;
   } else {
 
-    //props comes from Line 3 on client -->components --> students.js
     return (
       <div className="Container">
         <h2>The weather in {weather.name} is currently:</h2>
@@ -38,15 +35,15 @@ const Weather = () => {
         <p><b>City:</b> {weather.name}</p>
         <p><b>Description: </b> {weather.weather[0].description}</p>
         <p><b>Temperature: </b> {weather.main.temp}</p>
-          <p>Feels Like: {weather.main.feels_like}</p>
+        <p><b>Feels Like: </b> {weather.main.feels_like}</p>
         <p><b>Max Temp: </b> {weather.main.temp_max}</p>
-          <p><b>Min Temp: </b> {weather.main.temp_min}</p>
-          <p><b>Humidity: </b> {weather.main.humidity}</p> 
+        <p><b>Min Temp: </b> {weather.main.temp_min}</p>
+        <p><b>Humidity: </b> {weather.main.humidity}</p>
         <p><b>Sunrise: </b> {weather.sys.sunrise}</p>
-          <p><b>Sunset: </b> {weather.sys.sunset}</p>
+        <p><b>Sunset: </b> {weather.sys.sunset}</p>
       </div>
     )
   };
 };
 
-  export default Weather;
+export default Weather;
