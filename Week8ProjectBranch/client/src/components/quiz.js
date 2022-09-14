ddb9f0a1b31c1ca33b3a1de7bd1ec36763e26ffe
xocaid <xocaid@gmail.com>
2022-09-13 17:08:02 -0700

    Added button functionality to flip through questions

--- Week8ProjectBranch/client/src/components/quiz.js
+++ Week8ProjectBranch/client/src/components/quiz.js
@@ -1,23 +1,44 @@
 import { useState, useEffect } from "react";
 
 const QuizQs = () => {
-  const [quizInfo, setQuizInfo] = useState(null);
+  //This is to access the hardcode data from index.js
+  const [quizInfo, setQuizInfo] = useState(0);
 
+  //This is to apply to map function, so that it can populate the current question at position[x] in array
+  //useState is at 0 because you want to start at the first question in the array
+  const [currentQuestion, setCurrentQuestions] = useState(0);
 
-const loadData = () => {
-  fetch('http://localhost:8080/api/quizinfo')
-  .then((response) => response.json())
-  .then(
-    (data) => {
-      console.log(data);
-      setQuizInfo(data);
-    }
-  );
-};
+  //This function is an onClick event for the answer buttons, when you click an answer, it will move on to the next question
+  //change the current question by one, create function
+  const handleAnswerButtonClick = () => {
+    const nextQuestion = currentQuestion + 1;
+    setCurrentQuestions(nextQuestion);
+  }
 
-useEffect(() => {
-  loadData();
-}, []);
+  // const loadData = () => {
+  //   fetch('http://localhost:8080/api/quizinfo')
+  //   .then((response) => response.json())
+  //   .then(
+  //     (data) => {
+  //       console.log(data);
+  //       setQuizInfo(data);
+  //     }
+  //   );
+  // };
+  const loadData = () => {
+    fetch('http://localhost:8080/api/hardcodequizinfo')
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          console.log(data);
+          setQuizInfo(data);
+        }
+      );
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
 
 
 
@@ -25,9 +46,32 @@ useEffect(() => {
 
   return (
     <div>
-      <p>This is a Quiz Info placeholder.</p>
-      <p>Question: {quizInfo.results[0].question} </p>
-      <p>Response: {quizInfo.results[0].correct_answer}</p>
+      <h3>This is a Quiz Info placeholder.</h3>
+      <div className="question-section">
+
+        <div className="question-count">
+          Question 1{quizInfo.length}
+        </div>
+
+        <div className="quetion-text">
+          {/* PLACEHOLDER: This where the QUESTION TEXT will go. currentQuestion
+          replaces the number(0), number is now stored in the useState, making it dynamic  */}
+          {quizInfo[currentQuestion].questionText}
+        </div>
+
+        <div className="answer-section">
+          {/* Since the hardcode data is in the SERVER index.js, I need
+          to access it with quizInfo first. This map display the multiple
+          // choice answer options*/}
+          {quizInfo[currentQuestion].answerOptions.map(
+            (answerOptions) => (
+              <button onClick={handleAnswerButtonClick}>{answerOptions.answerText}</button>))}
+        </div>
+
+      </div>
+      {/* Below is for the infomration from the API */}
+      {/* <p>Question: {quizInfo.results[0].question} </p>
+      <p>Response: {quizInfo.results[0].correct_answer}</p> */}
     </div>
   )
 }
--- Week8ProjectBranch/server/index.js
+++ Week8ProjectBranch/server/index.js
@@ -12,13 +12,54 @@ app.get('/', (req, res) => {
   res.json('This is the backend.')
 });
 
-app.get('/api/quizinfo', (req, res) => {
-  const URL = `https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple`;
-  fetch(URL)
-    .then((response) => response.json())
-    .then((data) => {
-      res.send(data);
-    });
-})
+//Api being received from the API
+// app.get('/api/quizinfo', (req, res) => {
+//   const URL = `https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple`;
+//   fetch(URL)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       res.send(data);
+//     });
+// })
+
+app.get('/api/hardcodequizinfo', (req, res) => {
+  const QUIZINFO = [
+    {questionText: 'Who is 626?',
+    answerOptions: [
+      {answerText: 'Stitch' , isCorrect: true},
+      {answerText: 'Mickey Mouse', isCorrect: false},
+      {answerText: 'Donald Duck', isCorrect: false},
+      {answerText: 'Nani', isCorrect: false},
+      {answerText: 'Gru', isCorrect: false},//This is a 5th option, can be removed
+    ],
+  },
+  {questionText: 'Who is the US President?',
+  answerOptions: [
+    {answerText: "Donald Trump", isCorrect: false},
+    {answerText: 'Barack Obama', isCorrect: false},
+    {answerText: 'Joe Biden', isCorrect: true},
+    {answerText: 'George Washington', isCorrect: false},
+  ],
+},
+{questionText: 'Where is Los Angeles?',
+answerOptions: [
+  {answerText: 'New York', isCorrect: false},
+  {answerText: 'Texas', isCorrect: false},
+  {answerText: 'Florida', isCorrect: false},
+  {answerText: 'California', isCorrect: true},
+],
+},
+{questionText: 'Where does Santa Claus live?',
+answerOptions: [
+  {answerText: 'North Pole', isCorrect: true},
+  {answerText: 'South Pole', isCorrect: false},
+  {answerText: 'West Pole', isCorrect: false},
+  {answerText: 'East Pole', isCorrect: false},
+],
+},
+  ]
+  res.json(QUIZINFO);
+});
+
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}.`));
