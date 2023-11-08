cbfa2d6e1bd82c1b9f2806fc11196d1c55a5c32e
Xochitl <xocaid@gmail.com>
2023-11-08 15:22:52 -0800

    updating db.sql file

--- Week9_Eventonica/server/db/db.sql
+++ Week9_Eventonica/server/db/db.sql
@@ -1,11 +1,12 @@
--- TERMINAL COMMAND: pg_dump -d nameOfDatabase; 
+-- TERMINAL COMMAND: pg_dump -d nameOfDatabase --inserts --no-owner --clean | pbcopy; 
+-- TERMINAL COMMAND in folder: pg_dump -d nameOfDatabase --inserts --no-owner --clean > db.sql; 
 -- Purpose: pg_dump creates a back-up of your database by extracting it(database) into a script file.
 -- Create nameOfFile.sql file(in db folder) on server side, paste & save pg_dump script file, need to comment out info after "SET"
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.5 (Homebrew)
--- Dumped by pg_dump version 14.5 (Homebrew)
+-- Dumped from database version 16.0
+-- Dumped by pg_dump version 16.0
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,12 +19,22 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+ALTER TABLE ONLY public.users DROP CONSTRAINT users_pkey;
+ALTER TABLE ONLY public.users DROP CONSTRAINT users_email_key;
+ALTER TABLE ONLY public.events DROP CONSTRAINT events_pkey;
+ALTER TABLE ONLY public.events DROP CONSTRAINT events_description_key;
+ALTER TABLE public.users ALTER COLUMN id DROP DEFAULT;
+ALTER TABLE public.events ALTER COLUMN id DROP DEFAULT;
+DROP SEQUENCE public.users_id_seq;
+DROP TABLE public.users;
+DROP SEQUENCE public.events_id_seq;
+DROP TABLE public.events;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: events; Type: TABLE; Schema: public; Owner: tpl522_10
+-- Name: events; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.events (
@@ -35,10 +46,8 @@ CREATE TABLE public.events (
 );
 
 
-ALTER TABLE public.events OWNER TO tpl522_10;
-
 --
--- Name: events_id_seq; Type: SEQUENCE; Schema: public; Owner: tpl522_10
+-- Name: events_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.events_id_seq
@@ -50,17 +59,15 @@ CREATE SEQUENCE public.events_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.events_id_seq OWNER TO tpl522_10;
-
 --
--- Name: events_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tpl522_10
+-- Name: events_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.events_id_seq OWNED BY public.events.id;
 
 
 --
--- Name: users; Type: TABLE; Schema: public; Owner: tpl522_10
+-- Name: users; Type: TABLE; Schema: public; Owner: -
 --
 
 CREATE TABLE public.users (
@@ -70,10 +77,8 @@ CREATE TABLE public.users (
 );
 
 
-ALTER TABLE public.users OWNER TO tpl522_10;
-
 --
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: tpl522_10
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
 CREATE SEQUENCE public.users_id_seq
@@ -85,69 +90,62 @@ CREATE SEQUENCE public.users_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.users_id_seq OWNER TO tpl522_10;
-
 --
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: tpl522_10
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
 ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- Name: events id; Type: DEFAULT; Schema: public; Owner: tpl522_10
+-- Name: events id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.events ALTER COLUMN id SET DEFAULT nextval('public.events_id_seq'::regclass);
 
 
 --
--- Name: users id; Type: DEFAULT; Schema: public; Owner: tpl522_10
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 
 
 --
--- Data for Name: events; Type: TABLE DATA; Schema: public; Owner: tpl522_10
+-- Data for Name: events; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.events (id, name, description, date, category) FROM stdin;
-1111	Rose Apothecary Launch Event	Friends & Family Sale	12/03/22	Social
-549	Schitt's Creek Vacation	Visiting Patrick for the day	10/13/22	Personal
-\.
+INSERT INTO public.events VALUES (1111, 'Rose Apothecary Launch Event', 'Friends & Family Sale', '12/03/22', 'Social');
+INSERT INTO public.events VALUES (549, 'Schitt''s Creek Vacation', 'Visiting Patrick for the day', '10/13/22', 'Personal');
 
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: tpl522_10
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-COPY public.users (id, name, email) FROM stdin;
-9	David Rose	davidR@screek.com
-10	Moira Rose	moiraR@screek.com
-11	Johnny Rose	johnnyR@screek.com
-12	Stevie Budd	stevieB@screek.com
-13	Jocelyn Schitt	jocelynS@screek.com
-17	sdvdbv	sdvwe@aol.com
-\.
+INSERT INTO public.users VALUES (9, 'David Rose', 'davidR@screek.com');
+INSERT INTO public.users VALUES (10, 'Moira Rose', 'moiraR@screek.com');
+INSERT INTO public.users VALUES (11, 'Johnny Rose', 'johnnyR@screek.com');
+INSERT INTO public.users VALUES (12, 'Stevie Budd', 'stevieB@screek.com');
+INSERT INTO public.users VALUES (13, 'Jocelyn Schitt', 'jocelynS@screek.com');
 
 
 --
--- Name: events_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tpl522_10
+-- Name: events_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
 SELECT pg_catalog.setval('public.events_id_seq', 1, true);
 
 
 --
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: tpl522_10
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
 SELECT pg_catalog.setval('public.users_id_seq', 17, true);
 
 
 --
--- Name: events events_description_key; Type: CONSTRAINT; Schema: public; Owner: tpl522_10
+-- Name: events events_description_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.events
@@ -155,7 +153,7 @@ ALTER TABLE ONLY public.events
 
 
 --
--- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: tpl522_10
+-- Name: events events_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.events
@@ -163,7 +161,7 @@ ALTER TABLE ONLY public.events
 
 
 --
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: tpl522_10
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
@@ -171,7 +169,7 @@ ALTER TABLE ONLY public.users
 
 
 --
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: tpl522_10
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.users
